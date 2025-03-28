d8279646ab2b35e08b2806f6befff753bfc6f6da
Ju5t1nLi <justin.li3@ontariotechu.net>
2025-03-28 12:51:23 -0400

    more comments

--- Assets/Dialogue/DialogueManager.gd
+++ Assets/Dialogue/DialogueManager.gd
@@ -1,33 +1,39 @@
 extends Node
 
-@onready var text_box_scene = preload("res://Assets/Dialogue/Textbox.tscn")
+@onready var text_box_scene = preload("res://Assets/Dialogue/Textbox.tscn") #reference to textbox scene
 
+#Paramters for dialog within the textbox (includes the lines, index of which line of dialogue is playing, and more)
 var dialog_lines: Array[String] = []
 var current_line_index = 0
 var dialog_close_delay = 0.0
 var text_box
 var text_box_position: Vector2
 
+#Booleans used to track if dialog is in progress, or needs to continue
 var is_dialog_active = false
 var can_advance_line = false
 
-signal dialog_finished()
+signal dialog_finished()#Signal that activates when dialog is finished
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS  # Dialogue should process even when game is paused
 
-
+#Function to start dialog with an object
 func start_dialog(position: Vector2, lines: Array[String]):
-	if is_dialog_active:
+	if is_dialog_active: #Ensure dialog is actually active
 		return
-		
+	
+	#Initialize the text box position and lines of text, and show the text box with these variables	
 	dialog_lines = lines
 	text_box_position = position
 	_show_text_box()
 	
+	#Set flag true indicating that dialog is about to start
 	is_dialog_active = true
 	
+#Function to show the text box for dialog
 func _show_text_box():
+	#Instantiate the textbox to hold dialog above the interaction area. Display text line by line based on the lines given
 	text_box = text_box_scene.instantiate()
 	text_box.finished_displaying.connect(_on_text_box_finished_displaying)
 	get_tree().root.add_child(text_box)
@@ -35,13 +41,17 @@ func _show_text_box():
 	text_box.display_text(dialog_lines[current_line_index])
 	can_advance_line = false
 	
+#When the text box is done displaying a line of text, set flag to true to continue dialog
 func _on_text_box_finished_displaying():
 	can_advance_line = true
 
+#Function for input handling
 func _unhandled_input(event: InputEvent) -> void:
+	#Once dialog is done, remove the textbox and show the next line
 	if event.is_action_pressed("advance_dialog") and is_dialog_active and can_advance_line:
 		text_box.queue_free()
 		
+		#Display each line of dialog, continuing through the indexes until the lines are done
 		current_line_index += 1
 		if current_line_index >= dialog_lines.size():
 			is_dialog_active = false
@@ -50,4 +60,4 @@ func _unhandled_input(event: InputEvent) -> void:
 			dialog_close_delay = Time.get_ticks_msec() + 200
 			return
 		
-		_show_text_box()
+		_show_text_box() #Show the text box
--- Assets/Dialogue/textbox.gd
+++ Assets/Dialogue/textbox.gd
@@ -1,46 +1,59 @@
 extends MarginContainer
 
+#Instances for the label and timer for the textbox
 @onready var label = $MarginContainer/Label
 @onready var timer = $LetterDisplayTimer
 
-const MAX_WIDTH = 256
+const MAX_WIDTH = 256 #maximum width of the textbox
 
+#variables to hold the text and the index of each letter
 var text = ""
 var letter_index = 0
 
+#Variables for timing between letter, space, and punctuation as letters appear for the text
 var letter_time = 0.03
 var space_time = 0.06
 var punctuation_time = 0.2
 
-signal finished_displaying()
+signal finished_displaying() #Signal that actiates when the text is done being added to the textbox
 
+#Function to display the text
 func display_text(text_to_display: String):
+	#Set the label text
 	text = text_to_display
 	label.text = text_to_display
 	
+	#Create a minimum textbox size so it stays in the screen
 	await resized
 	custom_minimum_size.x = min(size.x, MAX_WIDTH)
 	
+	#Once max width is reached, wrap the text onto a new line
 	if size.x > MAX_WIDTH:
 		label.autowrap_mode = TextServer.AUTOWRAP_WORD
 		await resized #wait for x
 		await resized #wait for y
 		custom_minimum_size.y = size.y
-		
+	
+	#Calculate position of text box to display above interacted object
 	global_position.x -= size.x / 2
 	global_position.y -= size.y + 24
 	
+	#Rest label text if there was any, and display the letters one at a time
 	label.text = ""
 	_display_letter()
 	
+#Function that displays each letter of the text
 func _display_letter():
+	#Set the text to each letter of the line of dialog
 	label.text += text[letter_index]
 	
+	#Move to the next letter until it reaches the end of the line of dialog
 	letter_index += 1
 	if letter_index >= text.length():
-		finished_displaying.emit()
+		finished_displaying.emit() #Emit signal indicating the text is done displaying
 		return
-		
+	
+	#Add timing delays between each character so that text doesn't appear all at once
 	match text[letter_index]:
 		"!", ".", ",", "?":
 			timer.start(punctuation_time)
@@ -49,15 +62,6 @@ func _display_letter():
 		_:
 			timer.start(letter_time)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
+#Display letter once the previous letter is done displaying into the textbox
 func _on_letter_display_timer_timeout() -> void:
 	_display_letter()
